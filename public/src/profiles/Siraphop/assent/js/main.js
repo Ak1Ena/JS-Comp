0380a6d506e687138f45e859dc08f8ad69432ee6
Ak1Ena <poomzahathothai@gmail.com>
2025-08-12 12:06:27 +0700

    add animation&card

--- public/src/profiles/Siraphop/assent/js/main.js
+++ public/src/profiles/Siraphop/assent/js/main.js
@@ -1,3 +1,4 @@
+// JavaScript
 const names = ['SIRAPHOP', 'FULLSTACK DEVELOPER'];
 const nameEl = document.getElementById('name');
 
@@ -5,15 +6,53 @@ let currentIndex = 0;
 
 function changeName() {
   nameEl.classList.add('fade-out');
+
   setTimeout(() => {
-    nameEl.textContent = names[currentIndex];
+    let text = names[currentIndex];
+
+    if (currentIndex === 1) {
+      // แยก "FULLSTACK" กับ "DEVELOPER" แล้วใส่สี
+      const words = text.split(" ");
+      const colored = `<span class="blue">${words[0]}</span> ${words[1]}`;
+      nameEl.innerHTML = colored;
+    } else {
+      nameEl.textContent = text;
+    }
+
     currentIndex = (currentIndex + 1) % names.length;
     nameEl.classList.remove('fade-out');
   }, 2000);
 }
 
-// เริ่มเปลี่ยนชื่อครั้งแรก
+
 changeName();
 
-// เปลี่ยนชื่ออัตโนมัติทุก 3 วินาที
+// เปลี่ยนชื่อทุก 5 วินาที
 setInterval(changeName, 5000);
+
+
+//card
+const card = document.getElementsByClassName('card');
+
+// card.addEventListener('mouseover', (e) => {
+//   const rect = card.getBoundingClientRect();
+//   const x = e.clientX - rect.left; // mouse X within card
+//   const y = e.clientY - rect.top;  // mouse Y within card
+
+//   const centerX = rect.width / 2;
+//   const centerY = rect.height / 2;
+
+//   const deltaX = (x - centerX) / centerX;
+//   const deltaY = (y - centerY) / centerY;
+
+//   const maxTilt = 15; // degrees
+
+//   const tiltX = maxTilt * deltaY * -1; // invert for natural tilt
+//   const tiltY = maxTilt * deltaX;
+
+//   card.style.transform = `rotateX(${tiltX}deg) rotateY(${tiltY}deg)`;
+// });
+
+// card.addEventListener('mouseleave', () => {
+//   card.style.transform = 'rotateX(0deg) rotateY(0deg)';
+// });
