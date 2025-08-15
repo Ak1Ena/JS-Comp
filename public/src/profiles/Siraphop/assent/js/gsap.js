7cdba0329bd3f5455a1cb09cb42d0e656b563e7d
Ak1Ena <poomzahathothai@gmail.com>
2025-08-15 11:00:10 +0700

    fix respon

--- public/src/profiles/Siraphop/assent/js/gsap.js
+++ public/src/profiles/Siraphop/assent/js/gsap.js
@@ -16,32 +16,7 @@ const container = document.querySelector("#education");
 //   box.textContent = number;
 //   container.appendChild(box);
 // }
-const school = document.getElementById('school');
-const schoolTitle = document.getElementById('school-title')
-const grade = document.getElementById('grade')
-function updateBox(
-    title = "Unknown School", 
-    gradeValue = "N/A", 
-    visible = "visible", 
-    animation = "fadeInUp 0.2s ease-in-out forwards "
-) {
-    schoolTitle.textContent = title;
-    grade.textContent = "GPA : " + gradeValue;
-    school.style.visibility = visible;
 
-    // reset animation ก่อน เพื่อให้เรียกซ้ำก็เล่นได้
-    school.style.animation = "none";
-    void school.offsetWidth; // trigger reflow
-
-    school.style.animation = animation;
-}
-function hideBox() {
-  console.log("hide");
-  
-        school.style.animation = "none";
-    void school.offsetWidth; // trigger reflow
-    school.style.animation = "fadeOutDown 0.2s ease-in-out forwards";
-}
 
 gsap.registerPlugin(ScrollTrigger, DrawSVGPlugin, MotionPathPlugin, GSDevTools);
 
@@ -55,19 +30,12 @@ const pulses = gsap.timeline({
     ease: "elastic(2.5, 1)"
   }})
 .to(".ball02, .text01", {
-    onStart: () => updateBox("SawanananWitaya School"),
-    onReverseComplete: () => updateBox("SawanananWitaya School")
 }, 0.84) 
 .to(".ball03, .text02", {
-    onStart: () => updateBox("SawanananWitaya School","3.85")
-    ,onReverseComplete: () => updateBox("SawanananWitaya School","3.85")
 }, 1.36)
 .to(".ball04, .text03", {
-    onStart: () => updateBox("ChiangMai University","3.91")
-    ,onReverseComplete: () => updateBox("ChiangMai University","3.91")
 }, 1.92)
 .to(".ball05, .text04", {
-    onStart: () => updateBox("ChiangMai University")
 }, 2.48)
 
 const main = gsap.timeline({
@@ -76,9 +44,6 @@ const main = gsap.timeline({
     scrub: true,
     start: "top+=10 center",
     end: "bottom+=10 center",
-    onLeave: () => hideBox(), // เรียก hideBox() เมื่อย้ายออกจากพื้นที่
-    onLeaveBack: () => hideBox(), // เรียก hideBox() เมื่อย้อนเกินบนสุด
-    onEnterBack: () => updateBox("ChaingMai University") // ถ้าต้องการให้ fade in กลับเมื่อเลื่อนลง
   }
 })
 .to(".ball01", {autoAlpha:1, duration:0.05})
@@ -93,35 +58,6 @@ ScrollTrigger.create({
   trigger: "#svg",
   start: "bottom bottom",
 });
-
-document.querySelector('a[href="#app"]').addEventListener('click', (e) => {
-  hideBox();
-});
-document.querySelector('a[href="#about-me"]').addEventListener('click', (e) => {
-  hideBox();
-});
-document.querySelector('a[href="#education"]').addEventListener('click', (e) => {
-  hideBox();
-  main.seek(0);
-});
-document.querySelector('a[href="#skills"]').addEventListener('click', (e) => {
-  hideBox();
-});
-document.querySelector('a[href="#projects"]').addEventListener('click', (e) => {
-  hideBox();
-});
-document.querySelector('a[href="#contact"]').addEventListener('click', (e) => {
-  hideBox();
-});
-//GSDevTools.create({animation:main})
-
-
-
-
-
-
-
-
 
 
 
